8a6634baeb8414c43ea9fa242b3ad55916717a70
SpyridonLaz <lazanas.spy@gmail.com>
2023-02-02 15:02:04 +0200

    initial setup: basic models, settings.py, setting venv and env variables

---
+++ assessment/apps/projects/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+'''Provide a CRUD Project API. A project will contain a title, description, progress and
+finish date and will either be personal access (only the owner can view and edit it
+and it’s tasks) or public (Any registered user will be able to view the project and it’s
+tasks and only it’s owner will be able to edit it and it’s tasks). The progress will be
+the completion percentage of the tasks of the project and will be automatically
+updated (Using any calculation algorithm of your choice).'''
+
+class Projects(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    progress = models.IntegerField()
+    finish_date = models.DateField()
+    is_public = models.BooleanField()
+
+    def __str__(self):
+        return self.title
---
+++ assessment/apps/tasks/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from assessment.apps.projects.models import Projects
+
+# Create your models here.
+'''
+Provide a CRUD Task API. A task will contain a title, description, list of tags, parent project,
+progress and finish date. The progress will be the completion percentage of the task
+and will be updated manually from the project owner and when the task is
+completed, it will not be editable. The tags will be user-defined strings that will be
+used for the logical grouping of tasks under a project and will be passed optionally as
+a query parameter on the task read operations.'''
+
+
+class Tasks(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    progress = models.IntegerField()
+    finish_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Tags(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=100)
+    task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tag
